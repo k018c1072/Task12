aa8dd2a3dd9928128e569b53071cb0c8640be3cc
k018c1072 <k018c1072@it-neec.jp>
2020-06-29 14:06:20 +0900

    二回目のコミット

--- Dice.py
+++ Dice.py
@@ -1,7 +1,9 @@
+import random
+
+
 class Dice():
     def __init__(self, num):
         self.num = num
 
     def roll(self):
-        import random
         return random.randint(1, self.num)
--- Fortune.py
+++ Fortune.py
@@ -1,6 +1,8 @@
+import random
+
+
 class Fortune():
     kuji = ['大吉', '中吉', '小吉', '凶', '大凶']
 
     def draw(self):
-        import random
-        print(random.choice(kuji))
+        print(random.choice(self.kuji))
